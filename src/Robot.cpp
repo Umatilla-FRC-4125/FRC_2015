2ba646f7d50c06d377b9eb75b2de2c84dce0e477
Robotics <Robotics@L-U-ROBOTICS-3.usd.local>
2015-01-09 19:31:48 -0800

    Added Solenoid shifter.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,6 +5,7 @@ class Robot: public IterativeRobot
 private:
 	RobotDrive *myDrive;
 	Joystick *stick= new Joystick (0); // only joystick stick (0)
+	DoubleSolenoid *shift= new DoubleSolenoid (0,1);
 	void RobotInit()
 	{
 		//lw = LiveWindow::GetInstance();
@@ -29,8 +30,14 @@ private:
 	void TeleopPeriodic()
 	{
 		if(stick->GetRawAxis(2)){
-		myDrive->Drive(stick->GetRawAxis(2),stick->GetRawAxis(0));
+			myDrive->Drive(stick->GetRawAxis(2),stick->GetRawAxis(0));
 		}else{myDrive->Drive(stick->GetRawAxis(3),stick->GetRawAxis(0));}
+		if(stick->GetRawButton(1)){
+			shift->Set(DoubleSolenoid::kForward);
+		}
+		else if(stick->GetRawButton(2)){
+			shift->Set(DoubleSolenoid::kReverse);
+		}
 	}
 
 	void TestPeriodic()
