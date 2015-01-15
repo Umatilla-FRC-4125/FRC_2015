a0b35bfa6e3c9e8087da03bbe8a24c9515d7a6a3
Robotics <Robotics@L-U-ROBOTICS-3.usd.local>
2015-01-14 18:05:48 -0800

    Added Talons

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -3,13 +3,35 @@
 class Robot: public IterativeRobot
 {
 private:
-	RobotDrive *myDrive;
+	//Joysticks
 	Joystick *stick= new Joystick (0); // only joystick stick (0)
+	//Solenoids
 	DoubleSolenoid *shift= new DoubleSolenoid (0,1);
+	//Drive stuff
+	RobotDrive *myDrive;
+	Talon *top_left = new Talon(0);
+	Talon *top_right = new Talon(1);
+	Talon *bottom_left = new Talon(2);
+	Talon *bottom_right = new Talon(3);
+	//Sensors
+	Encoder *right_encoder = new Encoder(0, 1, false);
+	Encoder *left_encoder = new Encoder(2, 3, true);
 	void RobotInit()
 	{
 		//lw = LiveWindow::GetInstance();
-		myDrive = new RobotDrive(0,1);
+		myDrive = new RobotDrive(top_left, bottom_left, top_right, bottom_right);
+		right_encoder->SetMaxPeriod(.1);
+		right_encoder->SetMinRate(10);
+		left_encoder->SetMaxPeriod(.1);
+		left_encoder->SetMinRate(10);
+		right_encoder->SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 3);
+		right_encoder->SetReverseDirection(true);
+		right_encoder->SetSamplesToAverage(7);
+				//C = 28.26
+		left_encoder->SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 3);
+		left_encoder->SetReverseDirection(true);
+		left_encoder->SetSamplesToAverage(7);
+
 	}
 
 	void AutonomousInit()
@@ -38,6 +60,7 @@ private:
 		else if(stick->GetRawButton(2)){
 			shift->Set(DoubleSolenoid::kReverse);
 		}
+
 	}
 
 	void TestPeriodic()
